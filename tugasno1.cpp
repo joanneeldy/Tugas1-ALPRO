4fb4cd89871f4048a1f1025f41caf8b6a0599f79
Joanneeldy <joannelthy@gmail.com>
2021-11-28 16:41:09 +0700

    menambah analisa

--- tugasno1.cpp
+++ tugasno1.cpp
@@ -31,6 +31,9 @@ int main()
     return 0;
 }
 
+
+//OUTPUT
+
 //Masukkan nama: Joanne Landy Tantreece
 //Masukkan umur: 18
 //Masukkan tempat lahir: Batam
@@ -41,4 +44,52 @@ int main()
 //Tempat Lahir: Batam
 //Berat Badan: 48.5 KG 
 
-//analisa
+
+//ANALISA PROGRAM
+
+//#include <iostream> merupakan deklarasi header 
+//file/preprocessor standar baru yang merupakan file berisi 
+//fungsi yang mendukung eksekusi instruksi tertentu dalam C++ dan 
+//telah dikompilasi sebelumnya.
+
+//#include <string> merupakan deklarasi header untuk pemrosesan string 
+//yang merupakan sederetan simbol yang tidak tertentu panjangnya.
+
+//using namespace std; adalah instruksi terhadap compiler untuk menggunakan 
+//semua fungsi yang terkait untuk kerangka yang sama, fungsinya untuk
+//meminimalisir kesalahan akibat tidak dikenalinya fungsi cout dan cin.
+
+//int main() harus ada dalam pemrograman C++, memiliki arti main program 
+//atau fungsi utama mengembalikan nilai int secara default, 
+//akan mengembalikan nilai 0.
+//int main() harus diawali dengan { dan diakhiri dengan } 
+//berisi instruksi yang akan dieksekusi berikut 
+//deklarasi variabel (variabel lokal) yang diperlukan.
+
+//string nama; string merepresentasikan data berupa karakter (char)
+
+//int umur; int = integer merupakan tipe data yang menampung angka bulat
+//positif maupun negatif
+
+//string lahir; string merepresentasikan data berupa karakter (char)
+
+//float berat; float = floating point untuk data bilangan yang mengandung
+//angka di belakang koma
+
+//cout<< - perintah cout merupakan sebuah objek dalam C++ yang digunakan 
+//untuk mengarahkan data ke dalam standar output (cetak pada layar)
+
+//getline() - digunakan untuk menginput lebih dari 1 kata atau yang 
+//menggunakan spasi
+
+//cin>> - perintah cin diguunakan untuk memasukkan data
+
+//cin.ignore(); berguna untuk meng-ignore sebuah new line
+
+//tanda \n digunakan untuk pindah baris
+
+//tanda semicolon (;) digunakan untuk mengakhiri sebuah pernyataan.
+
+//setiap pernyataan wajib diakhiri dengan sebuah tanda semicolon.
+
+//return 0; untuk keluar dari function dan mengabaikan segala hal.
